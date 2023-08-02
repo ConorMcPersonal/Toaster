e77448d1b25fd69f28578970e84aaa11cdbf8cd0
McMenamin <csm@renre.com>
2023-08-02 19:28:25 +0100

    Tidy up - bit shifting

--- SplitFiles/slot_monitor.c
+++ SplitFiles/slot_monitor.c
@@ -14,7 +14,10 @@
 #include util.h
 
 SlotMonitor* get_slot_monitor(int x, int y) {
+    //Spectrum screen pixel arithmetic - three sections of 8 lines of 32 (8x8) characters.
+    //All first rows, then all second rows and so on...
     long screenOffset = 32 * 8 * 8 * ((y - 1) / 8) + 32 * 8 * ((y -1) % 8) + x - 1;
+    //Attributes a bit easier
     long attrOffset = (y - 1) * 32 + x - 1;
     SlotMonitor slotMon = {
         (unsigned char)x,
@@ -22,28 +25,39 @@ SlotMonitor* get_slot_monitor(int x, int y) {
         void *(SCREENSTART + screenOffset),
         void *(ATTRSTART   + attrOffset),
     };
-    //Set up attributes
+    //Set up attributes - Blue x2, Yellow x2, Red x2, Black x2 - all white paper
     unsigned char* attrs = '\x39\x39\x3E\x3E\x3A\x3A\x38\x38';
     memcpy(slotMon.startAttributes, attrs, 7);
-    memcpy(slotMon.startAttributes + 32, attrs, 7);
+    memcpy(slotMon.startAttributes + 32, attrs, 7); //Next row down
     return *slotMon;
 }
 
+// This function writes the bytes for the line into buffer
 void value_to_line(unsigned char* buffer, int byte_count, int value, int range, int min) {
     //How many bits to set
     int bitsToSet = int(MIN((long)range, MAX(0, (long)value - (long)min))
                      * ((long)byte_count * 8) / (long)range);
     int i;
-    for (i = 1; i <= byte_count; ++i) {
-        if (bitsToSet >= i * 8) {
+    for (i = 0; i < byte_count; ++i) {
+        if (bitsToSet >= (i + 1) * 8) {
             buffer[i] = \xFF;
-        } elif (bitsToSet <= (i - 1) * 8) {
+        } elif (bitsToSet <= i * 8) {
             buffer[i] = \x00;
         } else {
-            buffer[i] = !(bitsToSet - (i * 8))
+            buffer[i] = \xFF << (8 - (bitsToSet - i * 8))
         }
     }
     return;
+}
+
+void line_to_mem(void* targetAddr, void* source, long size, long increment) {
+    int i;
+    int offset = 0;
+    for (i = 0; i < 3; ++i) {
+        memcpy(targetAddr + offset, source, size);
+        offset += increment;
+    }
+    return offset;
 }
 
 void draw_slot(SlotMonitor* slotMon, SlotState* slot) {
@@ -52,28 +66,32 @@ void draw_slot(SlotMonitor* slotMon, SlotState* slot) {
     int i;
     // Draw lines for Slot temp/power, Bread moisture/toastedness
     value_to_line(&line, 7, slot->temperature, 200, 0)
-    for (i = 0; i < 3; ++i) {
-        memcpy(slotMon->startPixels + lineInc, &line, 7);
-        lineInc += 256;
-    }
+    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256)
+    //for (i = 0; i < 3; ++i) {
+    //    memcpy(slotMon->startPixels + lineInc, &line, 7);
+    //    lineInc += 256;
+    //}
     lineInc += 256;
-    value_to_line(&line, 7, slot->power, 200, 0)
-    for (i = 0; i < 3; ++i) {
-        memcpy(slotMon->startPixels + lineInc, &line, 7);
-        lineInc += 256;
-    }
+    value_to_line(&line, 7, slot->power, 200, 0);
+    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
+    //for (i = 0; i < 3; ++i) {
+    //    memcpy(slotMon->startPixels + lineInc, &line, 7);
+    //    lineInc += 256;
+    //}
     //First pair done
     lineInc = 32;
-    value_to_line(&line, 7, slot->bread->moisture, 200, 0)
-    for (i = 0; i < 3; ++i) {
-        memcpy(slotMon->startPixels + lineInc, &line, 7);
-        lineInc += 256;
-    }
+    value_to_line(&line, 7, slot->bread->moisture, 200, 0);
+    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
+    //for (i = 0; i < 3; ++i) {
+    //    memcpy(slotMon->startPixels + lineInc, &line, 7);
+    //    lineInc += 256;
+    //}
     lineInc += 256;
-    value_to_line(&line, 7, slot->bread->toastedness, 200, 0)
-    for (i = 0; i < 3; ++i) {
-        memcpy(slotMon->startPixels + lineInc, &line, 7);
-        lineInc += 256;
-    }
+    value_to_line(&line, 7, slot->bread->toastedness, 200, 0);
+    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
+    //for (i = 0; i < 3; ++i) {
+    //    memcpy(slotMon->startPixels + lineInc, &line, 7);
+    //    lineInc += 256;
+    //}
 }
 
