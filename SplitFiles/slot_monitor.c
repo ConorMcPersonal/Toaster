c1dca2abf7ac436e56213b3ba0a43c7fd3b095ef
unknown <conor.mcmenamin@gmail.com>
2023-09-05 00:15:49 +0100

    Hybrid graphics approach

--- SplitFiles/slot_monitor.c
+++ SplitFiles/slot_monitor.c
@@ -17,99 +17,72 @@
 #include "slot_monitor.h"
 #include "util.h"
 
-// This function writes the bytes for the line into buffer
-void value_to_line(unsigned char* buffer, int byte_count, int value, int range, int min) {
-    //How many bits to set
-    int bitsToSet = (int)(MIN((long)range, MAX(0, (long)value - (long)min))
-                     * ((long)byte_count * 8) / (long)range);
-    int i;
-    for (i = 0; i < byte_count; ++i) {
-        if (bitsToSet >= (i + 1) * 8) {
-            buffer[i] = 0xFF;
-        } else if (bitsToSet <= i * 8) {
-            buffer[i] = 0x00;
-        } else {
-            buffer[i] = 0xFF << (8 - (bitsToSet - i * 8));
-        }
-    }
-    return;
-}
+const unsigned char full[]  = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
+const unsigned char empty[] = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
 
-int line_to_mem(void* targetAddr, void* source, long size, long increment) {
-    int i;
-    int offset = 0;
-    unsigned char *hitHere = targetAddr;
-    for (i = 0; i < 3; ++i) {
-        memcpy(hitHere + offset, source, size);
-        offset += increment;
+void draw_bar(char* startAddr, int byteCount, int value, int range, int min) {
+    // How many bits need to be filled?
+    int bitsToSet = (MIN(range, MAX(0, value - min))
+                     * (byteCount * 8)) / range;
+    int fullBytes = MIN(bitsToSet / 8, byteCount - 1);
+    int emptyBytes = byteCount - 1 - fullBytes;
+    unsigned char extra = 0xFF << (8 - (bitsToSet - fullBytes * 8));
+    //printf(PRINTAT"\x05\x0B""%d %d %d %d    ", bitsToSet, fullBytes, emptyBytes, extra);
+    memcpy(startAddr, &full, fullBytes);
+    *(startAddr + fullBytes) = extra;
+    memcpy(startAddr + fullBytes + 1, &empty, emptyBytes);
+    //Now copy down
+    for (int i = 1; i < 3; ++i) {
+        memcpy(startAddr + 256 * i, startAddr, byteCount);
     }
-    return offset;
 }
 
 void draw_slot(SlotMonitor* slotMon, SlotState* slot) {
-    unsigned char line[8]; 
-    int lineInc = 0;
     // Draw lines for Slot temp/power, Bread moisture/toastedness
-    value_to_line(&(line[0]), 7, slot->temperature, 200, 0);
-    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
-
-    lineInc += 256;
-    value_to_line(&(line[0]), 7, slot->power, 200, 0);
-    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
+    draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase), 8, slot->temperature, 200, 0);
+    draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase + 4), 8, slot->power, 200, 0);
 
     //First pair done - on to bread
-    lineInc = 32;
     if (slot->bread) {
-        value_to_line(&(line[0]), 7, slot->bread->moisture, 200, 0);
+        draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase + 8), 8, slot->bread->moisture, 200, 0);
+        draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase + 12), 8, slot->bread->toastedness, 200, 0);
     } else {
-        value_to_line(&(line[0]), 7, 0, 200, 0);
+        draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase + 8), 8, 0, 200, 0);
+        draw_bar(zx_pxy2saddr(slotMon->xBase, slotMon->yBase + 12), 8, 0, 200, 0);
     }
-    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
-
-    lineInc += 256;
-    if (slot->bread) {
-        value_to_line(&(line[0]), 7, slot->bread->toastedness, 200, 0);
-    } else {
-        value_to_line(&(line[0]), 7, 0, 200, 0);
-    }
-    lineInc += line_to_mem(slotMon->startPixels + lineInc, &line, 7, 256);
-
 }
 
 SlotMonitor* get_slot_monitor(unsigned char x, unsigned char y, int slotIndex) {
-    long screenOffset;
-    long attrOffset;
-
-    //Spectrum screen pixel arithmetic - three sections of 8 lines of 32 (8x8) characters.
-    //All first rows, then all second rows and so on...
-    screenOffset = 32 * 8 * 8 * ((y - 1) / 8) + 32 * ((y - 1) % 8) + x - 1;
-    //Attributes a bit easier
-    attrOffset = (y - 1) * 32 + x - 1;
-
-    //printf("%d ", screenOffset);
-    //printf("%d ", attrOffset);
-    //printf("%d ", x);
-    //printf("%d\n", y);
-    SlotMonitor* slotMon = malloc(sizeof(SlotMonitor));
-    slotMon->xBase = x;
-    slotMon->yBase = y;
-    slotMon->startPixels =   (void *)(SCREENSTART + screenOffset);
-    slotMon->startAttributes = (void *)(ATTRSTART + attrOffset);
-    slotMon->draw_slot = &draw_slot;
-    //printf("%x   ", slotMon->startPixels);
-    //printf("%x\n", slotMon->startAttributes);
-
-    //Set up attributes - Blue x2, Magenta x2, Red x2, Black x2 - all with white paper
-    //const unsigned char* attrs = (unsigned char*)"\x79\x79\x7B\x7B\x7A\x7A\x78\x78"; //BRIGHT
-    const unsigned char* attrs = (unsigned char*)"\x39\x39\x3B\x3B\x3A\x3A\x38\x38"; //DIMMER
-    memcpy(slotMon->startAttributes, attrs, 7);
-    memcpy(slotMon->startAttributes + 32, attrs, 7); //Next row down
+    //Use x and y directly once only
     printf(PRINTAT"%c%c""Slot %d", x, y - 1, slotIndex);
+
+    SlotMonitor* slotMon = malloc(sizeof(SlotMonitor));
+    slotMon->xBase = (x - 1) * 8;
+    slotMon->yBase = (y - 1) * 8;
+    slotMon->draw_slot = &draw_slot;
+
+    const unsigned char* attrs = (unsigned char*)"\x39\x39\x3B\x3B\x3A\x3A\x38\x38"; //DIMMER
+    memcpy(zx_cxy2aaddr(x - 1, y - 1), attrs, 8);
+    memcpy(zx_cxy2aaddr(x - 1, y    ), attrs, 8); //Next row down
     return slotMon;
 }
 
 int main_slot_monitor()
 {
+  zx_cls(PAPER_WHITE);
+  char* startAddr = (char*)SCREENSTART + 256;
+  for (int j = 0; j < 10; ++j) {
+    int i;
+    for (i = 0; i < 250; ++i) {
+        draw_bar(startAddr, 16, i, 200, 0);
+        //for (int j = 0; j < 100; ++j) {}
+    } 
+    for (i; i > -10; --i) {
+        draw_bar(startAddr, 16, i, 200, 0);
+        //for (int j = 0; j < 100; ++j) {}
+    } 
+  }
+
   int i = 0;
   const char *alphabet = "0123456789ABCDEF";
   unsigned char *attr = (unsigned char *)ATTRSTART;
@@ -117,13 +90,13 @@ int main_slot_monitor()
   //Clear screen
   zx_cls(PAPER_WHITE);
 
-  for (i = 0; i < -256; ++i){
-//    printf("%c", alphabet[i % 16]);
+  for (i = 0; i < 256; ++i){
+    printf("%c", alphabet[i % 16]);
     *attr = i;
     attr++;
   }
   printf("\n");
-  slotMon = get_slot_monitor(3, 12, 1);//, 12l);
+  slotMon = get_slot_monitor(3, 17, 1);//, 12l);
   BreadState brd = {
     175,
     50,
