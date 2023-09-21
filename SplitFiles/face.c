dd27f4f2bce46774151fea259226932196aeff16
McMenamin Adrian (uic73892) <adrian.mcmenamin@conti-engineering.com>
2023-09-21 21:58:42 +0100

    shake it off

--- SplitFiles/face.c
+++ SplitFiles/face.c
@@ -29,82 +29,6 @@ void happyFace(const int x, const int y)
     cheeky_face(x, y, (char *)&faceData);
 }
 
-void OLDhappyFace(const int x, const int y)
-{
-    int px = (x - 1) * 8;
-    int py = (y - 1) * 8;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0x08;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x2;
-    *zx_pxy2saddr(px, py++) = 0x80;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x84;
-    *zx_pxy2saddr(px, py++) = 0x43;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x8;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x0;
-}
-
-void OLD_neutralFace(const int x, const int y)
-{
-    int px = (x - 1) * 8;
-    int py = (y - 1) * 8;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0x08;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x2;
-    *zx_pxy2saddr(px, py++) = 0x80;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x84;
-    *zx_pxy2saddr(px, py++) = 0x43;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x8;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x0;
-}
-
 void sadFace(const int x, const int y)
 {
     char faceData[] = { 0x0, 0x1, 0xe0, 0xe, 0x10, 0x10, 0x8, 0x20, 0x44,
@@ -113,43 +37,6 @@ void sadFace(const int x, const int y)
     cheeky_face(x, y, (char *)&faceData);
 }
 
-void OLDsadFace(const int x, const int y)
-{
-    int px = (x - 1) * 8;
-    int py = (y - 1) * 8;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0x8;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x2;
-    *zx_pxy2saddr(px, py++) = 0x80;
-    *zx_pxy2saddr(px + 8, py) = 0x4;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x84;
-    *zx_pxy2saddr(px, py++) = 0x43;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x8;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x0;
-}
 
 void angryFace(const int x, const int y)
 {
@@ -158,46 +45,6 @@ void angryFace(const int x, const int y)
         0x10, 0x10, 0xe0, 0xe, 0x0, 0x1, 0x0, 0x0 };
     cheeky_face(x, y, (char *)&faceData);
 }
-
-void OLDangryFace(const int x, const int y)
-{
-    int px = (x - 1) * 8;
-    int py = (y - 1) * 8;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0x8;
-    *zx_pxy2saddr(px, py++) = 0x20;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x40;
-    *zx_pxy2saddr(px, py++) = 0x4;
-    *zx_pxy2saddr(px + 8, py) = 0x84;
-    *zx_pxy2saddr(px, py++) = 0x42;
-    *zx_pxy2saddr(px + 8, py) = 0x04;
-    *zx_pxy2saddr(px, py++) = 0x40;
-    *zx_pxy2saddr(px + 8, py) = 0x84;
-    *zx_pxy2saddr(px, py++) = 0x43;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x44;
-    *zx_pxy2saddr(px, py++) = 0x44;
-    *zx_pxy2saddr(px + 8, py) = 0x88;
-    *zx_pxy2saddr(px, py++) = 0x23;
-    *zx_pxy2saddr(px + 8, py) = 0x10;
-    *zx_pxy2saddr(px, py++) = 0x10;
-    *zx_pxy2saddr(px + 8, py) = 0xe0;
-    *zx_pxy2saddr(px, py++) = 0xe;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x1;
-    *zx_pxy2saddr(px + 8, py) = 0x0;
-    *zx_pxy2saddr(px, py++) = 0x0;
-}
-
-
 
 void neutralFace(const int x, const int y)
 {
