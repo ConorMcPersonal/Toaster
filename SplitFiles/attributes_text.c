d413749f41601e6786e247a73aeec6ba0dfe9815
unknown <conor.mcmenamin@gmail.com>
2023-12-09 11:10:45 +0000

    Improve masking; can now write over prior text without ending up white

--- SplitFiles/attributes_text.c
+++ SplitFiles/attributes_text.c
@@ -106,10 +106,18 @@ AttrFont font5 = {
     (const char *)&h5
 };
 
-void attr_write(AttrFont* font, char letter, int x, int y, char colour) {
+void attr_write_base(AttrFont* font, char letter, int x, int y, char colour, int ink) {
+    char mask;
     char* draw;
     char row;
     int i, j;
+
+    if (ink) {
+        mask = 0xF8; //11111000
+    } else {
+        mask = 0xC7; //11000111
+    }
+
     switch (letter) {
         case 't':
             draw = (char *)font->t;
@@ -133,7 +141,7 @@ void attr_write(AttrFont* font, char letter, int x, int y, char colour) {
             for (i = 0; i < font->width; ++i) {
                 if (x + i < 32) {
                     if ((row & 0x80) == 0x80) {
-                        *zx_cxy2aaddr(x + i, y + j) |= (colour |= FLASH);
+                        *zx_cxy2aaddr(x + i, y + j) = (*zx_cxy2aaddr(x + i, y + j) & mask) | (colour | FLASH);
                     }
                     row = row<<1;
                 }
@@ -141,6 +149,11 @@ void attr_write(AttrFont* font, char letter, int x, int y, char colour) {
             ++draw;
         }
     }
+}
+
+void attr_write(AttrFont* font, char letter, int x, int y, char colour) {
+    int ink = colour < 7;
+    attr_write_base(font, letter, x, y, colour, ink);
 }
 
 char random_colour(int ink) {
@@ -162,11 +175,11 @@ int write_fat(char letter, int x, int y, char colour) {
     return (&font5)->width;
 }
 
-void write_block(int x1, int y1, int x2, int y2) {
+void write_block_base(int x1, int y1, int x2, int y2, char bgcolour) {
     int i, j;
     for (i = x1; i <= x2; ++i) {
         for (j = y1; j <= y2; ++j) {
-            *zx_cxy2aaddr(i, j) = INK_BLACK | PAPER_BLACK;
+            *zx_cxy2aaddr(i, j) = bgcolour;
         }
     }
     i = x1;
@@ -183,6 +196,10 @@ void write_block(int x1, int y1, int x2, int y2) {
 
 }
 
+void write_block(int x1, int y1, int x2, int y2) {
+    write_block_base(x1, y1, x2, y2, INK_BLACK | PAPER_BLACK);
+}
+
 int attractor() {
     AttrFont* use_font = &font5;
     int i = 0;
@@ -190,8 +207,6 @@ int attractor() {
     zx_cls(PAPER_BLACK | INK_BLACK);
 
     while (in_inkey() == 0) {
-        zx_border(INK_BLACK);
-        zx_cls(PAPER_BLACK | INK_BLACK);
         attr_write(use_font, 't', 1, 7, random_colour(1));
         attr_write(use_font, 'o', 7, 8, random_colour(1));
         attr_write(use_font, 'a', 13, 7, random_colour(1));
@@ -202,7 +217,6 @@ int attractor() {
         attr_write(use_font, 'o', 10, 10, random_colour(0));
         attr_write(use_font, 's', 17, 13, random_colour(0));
         attr_write(use_font, 't', 23, 11, random_colour(0));
-        //for (i = 0; i < 10000; ++i) {}
     }
     zx_border(INK_WHITE);
     zx_cls(PAPER_WHITE | INK_BLACK);
@@ -230,12 +244,15 @@ int main() {
     write_block(15, 16, 31, 22);
     wait_for_a_new_key();
 
+    write_block_base(15, 16, 31, 22, INK_WHITE | PAPER_WHITE);
+    wait_for_a_new_key();
+
     attractor();
 
-    while (1) {
-        zx_border(INK_BLACK);
-        zx_cls(PAPER_BLACK | INK_BLACK);
+    zx_border(INK_BLACK);
+    zx_cls(PAPER_BLACK | INK_BLACK);
 
+    while (1) {
         attr_write(use_font, 't', 1, 7, random_colour(1));
         attr_write(use_font, 'o', 7, 8, random_colour(1));
         attr_write(use_font, 'a', 13, 7, random_colour(1));
